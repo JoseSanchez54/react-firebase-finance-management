2fe5b1e5b5160d81eb1979a947d7f04b8a7bbde2
Jose Sánchez <drumstock@protonmail.com>
2023-10-18 13:08:53 +0200

    rt

--- src/App.js
+++ src/App.js
@@ -1,65 +1,61 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import useUser from './hooks/useUser'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import ScrollTop from './components/ScrollTop'
-import SectionType from './components/SectionType'
-import SectionSummary from './components/SectionSummary'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import useUser from "./hooks/useUser";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ScrollTop from "./components/ScrollTop";
+import SectionType from "./components/SectionType";
+import SectionSummary from "./components/SectionSummary";
 
 export default function App() {
-	const user = useUser(),
-			isUserSignedIn = user !== null;
-	return (
-		<BrowserRouter>
-			<Header />
+  const user = useUser(),
+    isUserSignedIn = user !== null;
+  return (
+    <BrowserRouter>
+      <Header />
 
-			<main>
-				<h1 className="txt-center">Finance Management</h1>
+      <main>
+        <h1 className="txt-center">Gestor patrimonial Beta 0.1.0</h1>
 
-				{isUserSignedIn ?
-					<Routes>
-						<Route
-							path='/'
-							element={[
-								<SectionType type='fun' />,
-								<SectionType type='life'/>,
-								<SectionType type='rent'/>
-							]}
-						/>
-						<Route
-							path='/investments'
-							element={[
-								<SectionType type='investments' />,
-								<SectionType type='roi' />
-							]}
-						/>
-						<Route
-							path='/earnings'
-							element={[
-								<SectionType type='earnings' />,
-								<SectionType type='job' />
-							]}
-						/>
-						<Route
-							path='/bank'
-							element={<SectionType type='bank' />}
-						/>
-						<Route
-							path='/summary'
-							element={<SectionSummary />}
-						/>
-						<Route
-							path='/*'
-							element={<h1 className="txt-center">404 Error - Page Not Found</h1>}
-						/>
-					</Routes>
-					:
-					<p className="txt-center">Sign in with Google to use the app.</p>
-				}
-			</main>
+        {isUserSignedIn ? (
+          <Routes>
+            <Route
+              path="/"
+              element={[
+                <SectionType type="fun" />,
+                <SectionType type="life" />,
+                <SectionType type="rent" />,
+              ]}
+            />
+            <Route
+              path="/investments"
+              element={[
+                <SectionType type="investments" />,
+                <SectionType type="roi" />,
+              ]}
+            />
+            <Route
+              path="/earnings"
+              element={[
+                <SectionType type="earnings" />,
+                <SectionType type="job" />,
+              ]}
+            />
+            <Route path="/bank" element={<SectionType type="bank" />} />
+            <Route path="/summary" element={<SectionSummary />} />
+            <Route
+              path="/*"
+              element={
+                <h1 className="txt-center">404 Error - Page Not Found</h1>
+              }
+            />
+          </Routes>
+        ) : (
+          <p className="txt-center">Entra con tu cuenta de google</p>
+        )}
+      </main>
 
-			<Footer />
-			<ScrollTop />
-		</BrowserRouter>
-	);
+      <Footer />
+      <ScrollTop />
+    </BrowserRouter>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,68 @@
-import { useRef } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import useIsOpen from '../hooks/useIsOpen'
-import useOutsideClick from '../hooks/useOutsideClick'
+import { useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+import useIsOpen from "../hooks/useIsOpen";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 function ActiveLink({ href, children, handleToggle }) {
-	const location = useLocation().pathname,
-			isActive = location === href;
-	return (
-		<Link to={href} className={isActive ? 'active' : ''} onClick={handleToggle}>
-			{children}
-		</Link>
-	)
+  const location = useLocation().pathname,
+    isActive = location === href;
+  return (
+    <Link to={href} className={isActive ? "active" : ""} onClick={handleToggle}>
+      {children}
+    </Link>
+  );
 }
 
 export default function Navbar({ signOut }) {
-	const refNavBar = useRef(),
-			[isOpen, handleToggle] = useIsOpen();
-	useOutsideClick({ val: isOpen, ref: refNavBar, handler: handleToggle });
-	return (
-		<>
-			<button
-				className="nav-toggle"
-				aria-controls={refNavBar}
-				aria-expanded={isOpen}
-				onClick={handleToggle}
-			>
-				<span className="sr-only">Menu</span>
-				<div className="bar1" />
-				<div className="bar2" />
-				<div className="bar3" />
-			</button>
+  const refNavBar = useRef(),
+    [isOpen, handleToggle] = useIsOpen();
+  useOutsideClick({ val: isOpen, ref: refNavBar, handler: handleToggle });
+  return (
+    <>
+      <button
+        className="nav-toggle"
+        aria-controls={refNavBar}
+        aria-expanded={isOpen}
+        onClick={handleToggle}
+      >
+        <span className="sr-only">Menu</span>
+        <div className="bar1" />
+        <div className="bar2" />
+        <div className="bar3" />
+      </button>
 
-			<nav id="navbar" ref={refNavBar} data-visible={isOpen}>
-				<ul>
-					<li>
-						<ActiveLink href="/" handleToggle={handleToggle}>Expense</ActiveLink>
-					</li>
-					<li>
-						<ActiveLink href="/investments" handleToggle={handleToggle}>Investments</ActiveLink>
-					</li>
-					<li>
-						<ActiveLink href="/earnings" handleToggle={handleToggle}>Earnings</ActiveLink>
-					</li>
-					<li>
-						<ActiveLink href="/bank" handleToggle={handleToggle}>Bank</ActiveLink>
-					</li>
-					<li>
-						<ActiveLink href="/summary" handleToggle={handleToggle}>Summary</ActiveLink>
-					</li>
-					<li>
-						<button onClick={signOut}>Sign Out</button>
-					</li>
-				</ul>
-			</nav>
-		</>
-	);
+      <nav id="navbar" ref={refNavBar} data-visible={isOpen}>
+        <ul>
+          <li>
+            <ActiveLink href="/" handleToggle={handleToggle}>
+              Gastos
+            </ActiveLink>
+          </li>
+          <li>
+            <ActiveLink href="/investments" handleToggle={handleToggle}>
+              Inversiones
+            </ActiveLink>
+          </li>
+          <li>
+            <ActiveLink href="/earnings" handleToggle={handleToggle}>
+              Ganancias
+            </ActiveLink>
+          </li>
+          <li>
+            <ActiveLink href="/bank" handleToggle={handleToggle}>
+              Banco
+            </ActiveLink>
+          </li>
+          <li>
+            <ActiveLink href="/summary" handleToggle={handleToggle}>
+              Sumario
+            </ActiveLink>
+          </li>
+          <li>
+            <button onClick={signOut}>Salir</button>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
