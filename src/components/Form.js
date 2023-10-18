3ab4ed8f91d9bee6503886e28cc692fd75c9a626
Jose Sánchez <drumstock@protonmail.com>
2023-10-18 14:04:59 +0200

    rt

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,55 +1,66 @@
-import { useState } from 'react';
-import { ref, set } from 'firebase/database';
-import { db, auth } from '../firebase';
-import { uid } from 'uid';
-import FormUI from './FormUI';
+import { useState } from "react";
+import { ref, set } from "firebase/database";
+import { db, auth } from "../firebase";
+import { uid } from "uid";
+import FormUI from "./FormUI";
 
 export default function Form({ type, handleChangeDate }) {
-	const initVals = { name: '', price: '', date: new Date().toISOString().split('T')[0] }
-	const [newExpense, setNewExpense] = useState(initVals);
+  const initVals = {
+    name: "",
+    price: "",
+    date: new Date().toISOString().split("T")[0],
+  };
+  const [newExpense, setNewExpense] = useState(initVals);
 
-	const handleChange = e => {
-		const { id, value } = e.target;
-		setNewExpense({ ...newExpense, [id]: value });
-	}
-	const handleReset = () => setNewExpense(initVals);
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setNewExpense({ ...newExpense, [id]: value });
+  };
+  const handleReset = () => setNewExpense(initVals);
 
-	const addUserExpense = e => {
-		e.preventDefault();
-		
-		const trimmedExpense = {};
-		for (const key in newExpense) {
-			if (Object.hasOwnProperty.call(newExpense, key)) {
-				trimmedExpense[key] = newExpense[key].trim();
-			}
-		}
-		if (Object.values(trimmedExpense).some(value => value === '')) {
-			return;
-		}
-			
-		const getExpenseDate = new Date(newExpense.date),
-				yearExpense = getExpenseDate.getFullYear(),
-				monthExpense = getExpenseDate.getMonth() + 1,
-				idExpense = uid();
+  const addUserExpense = (e) => {
+    e.preventDefault();
 
-		set(
-			ref(db, `/users/${auth.currentUser.uid}/${type}/${yearExpense}/${monthExpense}/${idExpense}/`),
-			{ ...trimmedExpense, id: idExpense }
-		)
-		.then(() => console.log(`${type !== 'earnings' ? 'Expense' : 'Earning'} saved successfully!`))
-		.catch(err => console.log(err));
+    const trimmedExpense = {};
+    for (const key in newExpense) {
+      if (Object.hasOwnProperty.call(newExpense, key)) {
+        trimmedExpense[key] = newExpense[key].trim();
+      }
+    }
+    if (Object.values(trimmedExpense).some((value) => value === "")) {
+      return;
+    }
 
-		setNewExpense(initVals);
-		handleChangeDate('month', monthExpense);
-	};
+    const getExpenseDate = new Date(newExpense.date),
+      yearExpense = getExpenseDate.getFullYear(),
+      monthExpense = getExpenseDate.getMonth() + 1,
+      idExpense = uid();
 
-	return (
-		<FormUI
-			handleSubmit={addUserExpense}
-			type={type}
-			valueData={newExpense}
-			handleChange={handleChange}
-			handleReset={handleReset}
-		/>
-	);
+    set(
+      ref(
+        db,
+        `/users/${auth.currentUser.uid}/${type}/${yearExpense}/${monthExpense}/${idExpense}/`
+      ),
+      { ...trimmedExpense, id: idExpense, mail: auth.currentUser.email }
+    )
+      .then(() =>
+        console.log(
+          `${type !== "earnings" ? "Expense" : "Earning"} saved successfully!`
+        )
+      )
+      .catch((err) => console.log(err));
+
+    setNewExpense(initVals);
+    handleChangeDate("month", monthExpense);
+  };
+
+  return (
+    <FormUI
+      handleSubmit={addUserExpense}
+      type={type}
+      valueData={newExpense}
+      handleChange={handleChange}
+      handleReset={handleReset}
+    />
+  );
 }
