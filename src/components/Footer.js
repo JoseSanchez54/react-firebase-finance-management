04c8f31f0bf4baca5fe40813e96276721faeb405
Jose Sánchez <drumstock@protonmail.com>
2023-10-18 12:44:22 +0200

    rt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,7 @@
 export default function Footer() {
-	return (
-		<footer>
-			<small>&copy; developed by</small>
-			<a href="https://travolgi.it" target="_blank" rel="noopener noreferrer">
-				<img src="/travolgi-logo.webp" alt="Travolgi logo" title="developed by Travolgi" />
-			</a>
-		</footer>
-	);
+  return (
+    <footer>
+      <small>&copy; developed by J.Sanchez</small>
+    </footer>
+  );
 }
